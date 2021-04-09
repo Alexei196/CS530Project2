642da232c35462b83834f8258b74680efff07566
Alexei196 <79891975+Alexei196@users.noreply.github.com>
2021-04-09 12:48:22 -0700

    Create import.cpp

---
+++ import.cpp
@@ -0,0 +1,11 @@
+/*  Andrew Rossman 
+    CS530 Project #2
+    Importing some stuff into the Link-Editor for use
+*/
+#include<iostream>
+#include<fstream>
+
+int main(int argc, char** argv) {
+    
+    return 0;
+}
